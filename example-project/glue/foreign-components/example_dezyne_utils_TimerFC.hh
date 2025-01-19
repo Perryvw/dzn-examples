a73a987786c0d48fb90dc3e42ac462a66b1620ef
Perryvw <perryvw@users.noreply.github.com>
2025-01-19 22:01:01 +0100

    Implement timer in example project using dzn pump

--- example-project/glue/foreign-components/example_dezyne_utils_TimerFC.hh
+++ example-project/glue/foreign-components/example_dezyne_utils_TimerFC.hh
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <dzn/pump.hh>
+
 #include "dzn-gen/timer.hh"
 
 namespace example::dezyne::utils {
@@ -9,19 +11,27 @@ class TimerFC : public skel::TimerFC {
 public:
     TimerFC(const dzn::locator& locator)
         : skel::TimerFC(locator)
+        , pump{locator.get<dzn::pump>()}
     {
     }
 
     void api_Start (std::chrono::milliseconds durationMs) override {
-
+        // Use the Dezyne pump
+        pump.handle(id(), durationMs.count(), [this]{
+            api.out.Timeout();
+        });
     }
     
     void api_Cancel () override {
-        
+        pump.remove(id());
     }
 
 private:
+    size_t id() const {
+        return reinterpret_cast<size_t>(this);
+    }
 
+    dzn::pump& pump;
 };
 
 }
